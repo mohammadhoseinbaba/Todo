4e133a16fcac5172d063e4cebcef575c36c46f28
mohammadhoseinbaba <sirhb77@gmail.com>
2024-11-30 16:53:01 +0100

    adding calender html

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import TodoList from './Components/Todolist'
 import Navbar from './Components/Navbar'
 import Timer from './Components/Timer'
 import Todo from './Components/Todo'
+import Calender from './Components/Calender'
 const App: React.FC = () => {
   return <div>
+    <Calender/>
     <TodoList />
   </div>
 }
---
+++ src/Components/Calender.tsx
@@ -0,0 +1,22 @@
+const Calender: React.FC = () => {
+    return <div className="container ">
+        <header>
+            <p>Monthesname</p>
+            <span>left</span>
+            <span>Right</span>
+        </header>
+        <div className="calenderbody">
+            <ul>
+                <li>SUN</li>
+                <li>MON</li>
+                <li>TUE</li>
+                <li>WED</li>
+                <li>THU</li>
+                <li>FRI</li>
+                <li>SAT</li>
+            </ul>
+            <ul></ul>
+        </div>
+    </div>
+}
+export default Calender
