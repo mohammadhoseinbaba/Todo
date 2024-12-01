66c86eafa0af1b3df50184398cac23618dedce52
mohammadhoseinbaba <sirhb77@gmail.com>
2024-12-01 18:45:33 +0100

    calender added

--- src/Components/Calender.tsx
+++ src/Components/Calender.tsx
@@ -1,18 +1,12 @@
+import React, { useState } from "react";
 import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 
-
-
 const Calender: React.FC = () => {
-    let date = new Date()
-    let year = date.getFullYear()
-    let month = date.getMonth()
+    const [date, setDate] = useState(new Date());
+    const year = date.getFullYear();
+    const month = date.getMonth();
 
-    const day = document.querySelector(".calenderDates")
-    const currDate = document.querySelector(".calenderCurrMonth")
-    const prenexIcons = document.querySelectorAll(".calenderNavigator span")
-    
-
-    const monthes = [
+    const months = [
         "January",
         "February",
         "March",
@@ -24,30 +18,97 @@ const Calender: React.FC = () => {
         "September",
         "October",
         "November",
-        "December"
-    ]
+        "December",
+    ];
 
+    const getDaysInMonth = () => {
+        const firstDayIndex = new Date(year, month, 1).getDay();
+        const lastDate = new Date(year, month + 1, 0).getDate();
+        const lastDayIndex = new Date(year, month + 1, 0).getDay();
+        const prevMonthLastDate = new Date(year, month, 0).getDate();
 
-    return <div className="container ">
-        <header className=" flex justify-between ">
-            <p className="calenderCurrMonth"></p>
-            <div className="calenderNavigator flex cursor-pointer ">
-                <span><GoChevronLeft /></span>
-                <span><GoChevronRight /></span>
+        const days: { date: number; isCurrentMonth: boolean; isToday: boolean }[] = [];
+
+        // Previous month's days
+        for (let i = firstDayIndex; i > 0; i--) {
+            days.push({
+                date: prevMonthLastDate - i + 1,
+                isCurrentMonth: false,
+                isToday: false,
+            });
+        }
+
+        // Current month's days
+        for (let i = 1; i <= lastDate; i++) {
+            days.push({
+                date: i,
+                isCurrentMonth: true,
+                isToday: i === date.getDate() && month === new Date().getMonth() && year === new Date().getFullYear(),
+            });
+        }
+
+        // Next month's days
+        for (let i = 1; i < 7 - lastDayIndex; i++) {
+            days.push({
+                date: i,
+                isCurrentMonth: false,
+                isToday: false,
+            });
+        }
+
+        return days;
+    };
+
+    const days = getDaysInMonth();
+
+    const handlePrevMonth = () => {
+        setDate(new Date(year, month - 1, 1));
+    };
+
+    const handleNextMonth = () => {
+        setDate(new Date(year, month + 1, 1));
+    };
+
+    return (
+        <div className="container">
+            <header className="flex justify-between">
+                <p className="calenderCurrMonth">
+                    {months[month]} {year}
+                </p>
+                <div className="calenderNavigator flex cursor-pointer">
+                    <span onClick={handlePrevMonth}>
+                        <GoChevronLeft />
+                    </span>
+                    <span onClick={handleNextMonth}>
+                        <GoChevronRight />
+                    </span>
+                </div>
+            </header>
+            <div className="calenderbody flex flex-col">
+                <ul className="flex">
+                    <li className="m-5">SUN</li>
+                    <li className="m-5">MON</li>
+                    <li className="m-5">TUE</li>
+                    <li className="m-5">WED</li>
+                    <li className="m-5">THU</li>
+                    <li className="m-5">FRI</li>
+                    <li className="m-5">SAT</li>
+                </ul>
+                <ul className="calenderDates grid grid-cols-7">
+                    {days.map((day, index) => (
+                        <li
+                            key={index}
+                            className={`m-5 ${
+                                day.isCurrentMonth ? "" : "text-gray-400"
+                            } ${day.isToday ? "bg-blue-500 text-white" : ""}`}
+                        >
+                            {day.date}
+                        </li>
+                    ))}
+                </ul>
             </div>
-        </header>
-        <div className="calenderbody flex">
-            <ul className="flex">
-                <li className="m-5">SUN</li>
-                <li className="m-5">MON</li>
-                <li className="m-5">TUE</li>
-                <li className="m-5">WED</li>
-                <li className="m-5">THU</li>
-                <li className="m-5">FRI</li>
-                <li className="m-5">SAT</li>
-            </ul>
-            <ul className="calenderDates"></ul>
         </div>
-    </div>
-}
-export default Calender
+    );
+};
+
+export default Calender;
